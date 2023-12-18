796e0dedfa7297373eddcbcc88b072e600047f1c
sixthson6 <sudais2408@gmail.com>
2023-12-18 22:45:24 +0000

    bubble_sort_1

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,7 +9,7 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-	/*int temp;*/
+	int temp;
 	bool swapped;
 
 	for (i = 0; i < size - 1; i++)
@@ -19,10 +19,9 @@ void bubble_sort(int *array, size_t size)
 		{
 			if (array[j] > array[j + 1])
 			{
-				/*temp = array[j];
+				temp = array[j];
 				array[j] = array[j + 1];
-				array[j + 1] = temp;*/
- 				swap(&array[j], &array[j + 1]);
+				array[j + 1] = temp;
 				swapped = true;
 				print_array(array, size);
 			}
