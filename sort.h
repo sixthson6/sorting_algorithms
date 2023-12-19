2978f1c97231e1993c020d6be43d6de42fdf7975
sixthson6 <sudais2408@gmail.com>
2023-12-19 20:56:48 +0000

    header

--- sort.h
+++ sort.h
@@ -15,15 +15,16 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 /* print_functions */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void swap(int *a, int *b);
+listint_t *create_listint(const int *array, size_t size);
 
 /* mandatory sorting algorithms */
 void bubble_sort(int *array, size_t size);
