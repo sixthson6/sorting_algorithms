d7204e7415c300ec5fc90e1412b921f1f58630a7
sixthson6 <sudais2408@gmail.com>
2023-12-19 21:31:51 +0000

    selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+
+/**
+ * selection_sort - implement selection sort
+ * @array: list to be sorted
+ * @size: size of array
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int temp;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[i] > array[j])
+			{
+				temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+			}
+		}
+		print_array(array, size);
+	}
+}
